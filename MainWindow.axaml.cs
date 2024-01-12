13ea78fa0b4f39019b4b3a19eaac7dc24991d92a
prdb <prdb@spb-rtk.ru>
2024-01-12 18:09:24 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using System;
+
+namespace Yandex
+{
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
